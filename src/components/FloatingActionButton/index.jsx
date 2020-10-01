2b04f61fae039a661888e715327030e8a5e35b0f
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-10-01 20:34:52 +0400

    implement floating action button

--- src/components/FloatingActionButton/index.jsx
+++ src/components/FloatingActionButton/index.jsx
@@ -1,30 +1,26 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Switch from "@material-ui/core/Switch";
-import Paper from "@material-ui/core/Paper";
-import Grow from "@material-ui/core/Grow";
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
 
+import { makeStyles } from "@material-ui/core/styles";
+import Grow from "@material-ui/core/Grow";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Tooltip from "@material-ui/core/Tooltip";
-import { Directions } from "@material-ui/icons";
-import FormGroup from "@material-ui/core/FormGroup";
 import Box from "@material-ui/core/Box";
-import Fade from "@material-ui/core/Fade";
 
-import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Tooltip from "@material-ui/core/Tooltip";
+
+import blue from "@material-ui/core/colors/blue";
+
+import Icon from "components/Icon/Icon";
+import { NavLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: 180,
-    marginBottom: "2000px"
+    marginBottom: "2000px",
   },
   container: {
-    display: "flex"
+    display: "flex",
     // flexDirection: "column"
   },
   // paper: {
@@ -32,17 +28,17 @@ const useStyles = makeStyles((theme) => ({
   // },
   tooltip: {
     backgroundColor: "transparent",
-    color: theme.palette.common.black
+    color: theme.palette.common.black,
   },
   svg: {
     width: 100,
-    height: 100
+    height: 100,
   },
   polygon: {
     fill: theme.palette.common.white,
     stroke: theme.palette.divider,
-    strokeWidth: 1
-  }
+    strokeWidth: 1,
+  },
 }));
 
 export default function FloatingActionButton() {
@@ -50,86 +46,23 @@ export default function FloatingActionButton() {
   const [checked, setChecked] = React.useState(false);
 
   const handleChange = () => {
-    console.log("kokok");
-
     setChecked((prev) => !prev);
   };
-
   return (
     <div className={classes.root}>
-      {/* <Switch  checked={checked} onChange={handleChange}/> */}
       <div className={classes.container}>
-        {/* <Tooltip
-        TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}
-          classes={classes}
-          // onOpen={handleChange}
-          title={
-            <Grow in={true} {...(checked ? { timeout: 3000 } : {})}>
-              <Box m={1}>
-                <FormControlLabel
-                  control={
-                    <Fab color="secondary" aria-label="add">
-                      <AddShoppingCartIcon />
-                    </Fab>
-                  }
-                />
-              </Box>
-            </Grow>
-
-    
-          }
-          // enterDelay={3000}
-          // leaveDelay={200}
-        >
-          <Box m={1}>
-            <FormControlLabel
-              control={
-                <Fab color="primary" aria-label="add">
-                  <AddIcon />
-                </Fab>
-              }
-            />
-          </Box>
-        </Tooltip> */}
-
-        <Box
-          // position="fixed"
-          // right="70px"
-          position="fixed"
-          bottom="20px"
-          right="20px"
-        >
-          <Grow in={checked} {...(checked ? { timeout: 2000 } : {})}>
-            <Box m={1}>
-              <FormControlLabel
-                control={
-                  <Fab color="secondary" aria-label="add">
-                    <AddShoppingCartIcon />
-                  </Fab>
-                }
-              />
-            </Box>
-          </Grow>
-
-          <Grow in={checked} {...(checked ? { timeout: 1500 } : {})}>
-            <Box m={1}>
-              <FormControlLabel
-                control={
-                  <Fab color="secondary" aria-label="add">
-                    <AddShoppingCartIcon />
-                  </Fab>
-                }
-              />
-            </Box>
-          </Grow>
-
+        <Box position="fixed" bottom="20px" right="20px">
           <Grow in={checked} {...(checked ? { timeout: 1000 } : {})}>
             <Box m={1}>
               <FormControlLabel
                 control={
-                  <Fab color="secondary" aria-label="add">
-                    <AddShoppingCartIcon />
-                  </Fab>
+                  <Tooltip title="Add Order" placement="left" arrow>
+                    <NavLink to="/order">
+                      <Fab color="secondary" aria-label="add">
+                        <Icon fill="white" name="fastIconOrder" />
+                      </Fab>
+                    </NavLink>
+                  </Tooltip>
                 }
               />
             </Box>
@@ -143,21 +76,26 @@ export default function FloatingActionButton() {
             <Box m={1}>
               <FormControlLabel
                 control={
-                  <Fab color="secondary" aria-label="add">
-                    <AddShoppingCartIcon />
-                  </Fab>
+                  <Tooltip title="Add Assortiment" placement="left" arrow>
+                    <NavLink to="/category">
+                      <Fab style={{ background: blue[500] }} aria-label="add">
+                        <Icon fill="white" name="fastIconCategory" />
+                      </Fab>
+                    </NavLink>
+                  </Tooltip>
                 }
               />
             </Box>
           </Grow>
-
           <Box m={1}>
             <FormControlLabel
               onClick={handleChange}
               control={
-                <Fab color="primary" aria-label="add">
-                  <AddIcon />
-                </Fab>
+                <Tooltip title="Add" placement="left" arrow>
+                  <Fab color="primary" aria-label="add">
+                    <AddIcon />
+                  </Fab>
+                </Tooltip>
               }
             />
           </Box>
--- src/routers/Routes.jsx
+++ src/routers/Routes.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 import { Switch, Redirect } from 'react-router-dom';
-import { SignIn, SignUp, Overview, Analytics } from 'containers';
+import {
+  Order,
+  SignIn,
+  SignUp,
+  History,
+  Overview,
+  Analytics,
+  Assortiment,
+} from "containers";
 
 import PrivateRoute from './PrivateRoute';
 import PublickRoute from './PublickRoute';
@@ -10,15 +18,18 @@ const AppRoutes = () => {
 	// console.log('AppRoutes RENDERRRRRRRRRRRR');
 
 	return (
-		<Switch>
-			<PublickRoute exact path="/login" component={SignIn} />
-			<PublickRoute path="/register" component={SignUp} />
-			<PrivateRoute path="/overview" component={Overview} />
-			<PrivateRoute path="/analytics" component={Analytics} />
-			{/* <Redirect to="/login" /> */}
-			{/* <Route component={() => <h2>404 Page Not Found</h2>} /> */}
-		</Switch>
-	);
+    <Switch>
+      <PublickRoute exact path="/login" component={SignIn} />
+      <PublickRoute path="/register" component={SignUp} />
+      <PrivateRoute path="/overview" component={Overview} />
+      <PrivateRoute path="/analytics" component={Analytics} />
+      <PrivateRoute path="/history" component={History} />
+      <PrivateRoute path="/order" component={Order} />
+      <PrivateRoute path="/category" component={Assortiment} />
+      <Redirect to="/login" />
+      {/* <Route component={() => <h2>404 Page Not Found</h2>} /> */}
+    </Switch>
+  );
 };
 
 export default AppRoutes;
