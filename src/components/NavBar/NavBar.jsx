6e08237765dbf62bdb7d06fc1fe19cf5f554671f
albertshkhyan <50060559+albertshkhyan@users.noreply.github.com>
2020-10-01 21:13:56 +0400

    fix error

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,60 +1,52 @@
-import React from "react";
+import React from 'react';
 
-import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
 
-import AppBarCollapse from "./AppBarCollapse";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import cl from "classnames";
-
+import AppBarCollapse from './AppBarCollapse';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import cl from 'classnames';
 
 const drawerWidth = 200;
 const useStyles = makeStyles((theme) => ({
-  appBar: {
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
-    },
-  },
+	appBar: {
+		[theme.breakpoints.up('sm')]: {
+			width: `calc(100% - ${drawerWidth}px)`,
+			marginLeft: drawerWidth,
+		},
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+		[theme.breakpoints.up('sm')]: {
+			display: 'none',
+		},
+	},
 }));
 
-const Navbar = ({ 
-    isAuth,
-    hanldeLogout,
-    handleDrawerToggle,
-}) => {
-  const classes = useStyles();
+const Navbar = ({ isAuth, hanldeLogout, handleDrawerToggle }) => {
+	const classes = useStyles();
 
-  return (
-    <>
-    <AppBar position="fixed" className={cl({ [classes.appBar]: isAuth  })}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            New Born
-          </Typography>
-          <AppBarCollapse
-            isAuth={isAuth}
-            onLogout={hanldeLogout}
-          />
-        </Toolbar>
-      </AppBar>
-    </>
-  );
+	return (
+		<>
+			<AppBar position="fixed" className={cl({ [classes.appBar]: isAuth })}>
+				<Toolbar>
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						edge="start"
+						onClick={handleDrawerToggle}
+						className={classes.menuButton}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" noWrap>
+						New Born
+					</Typography>
+					<AppBarCollapse isAuth={isAuth} onLogout={hanldeLogout} />
+				</Toolbar>
+			</AppBar>
+		</>
+	);
 };
 
 export default Navbar;
